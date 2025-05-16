6ba93d2fb14d9cc8908e83643688a4ebed95f9f7
Adam Anderson <adamtao@users.noreply.github.com>
2025-05-16 15:51:52 +0000

    Automating fixes to magellan and reveal within update-gem.sh script

--- update-gem.sh
+++ update-gem.sh
@@ -16,6 +16,13 @@ cd vendor/assets/javascripts
 echo '//= require foundation/foundation' > foundation.js
 for f in foundation/*.*.js; do echo "//= require $f" | sed 's/.js//' >> foundation.js ; done
 
+# fix broken magellan
+sed -i 's/href\*=#/href*=\"#\"/g' foundation/foundation.magellan.js;
+
+# fix broken reveal
+sed -i "s/css.top = \$(window).scrollTop() - el.data('offset')/css.top = \$(root_element).scrollTop() - el.data('offset')/" foundation/foundation.reveal.js;
+sed -i "s/top: \$(window).scrollTop() + el.data('css-top')/top: \$(root_element).scrollTop() + el.data('css-top')/" foundation/foundation.reveal.js;
+
 # echo "Now update version.rb"
 
 cd -
